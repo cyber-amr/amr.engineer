50b44c2b3e0868fd9fa2f80c8fa331231b641e59
Amr <amr@programmer.net>
2026-01-02 17:00:35 +0300

    Update introduction text in script.js

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@
 // /_/   \|_| |_| |_|_|    |____/ \___| \_/
 //
 const HLP = "℗℗℗℗_℗℗℗℗℗℗℗℗℗℗℗℗℗℗℗℗℗℗℗℗____℻℗℗℗/℗\\℗℗_℗__℗___℗℗_℗__℗℗|℗℗_℗\\℗℗_____℗℗℗__℻℗℗/℗=℗\\|℗'_℗`℗_℗\\|℗'__|℗|℗|℗|℗|/℗_℗\\℗\\℗/℗/℻℗/℗___℗|℗|℗|℗|℗|℗|℗|℗℗℗℗|℗|_|℗|℗℗__/\\℗V℗/℻/_/℗℗℗\\|_|℗|_|℗|_|_|℗℗℗℗|____/℗\\___|℗\\_/℻℗℻"
-  .concat("℻℗℻Hello! I'm Amr :3℻a Music enthusiast and hobbyist programmer.℻I specialize in crafting efficient and performant systems.℻My journey in tech is driven by curiosity and ecstasy for development.℻℗")
+  .concat("℻℗℻Hello! I'm Amr :3℻a software engineer and game developer.℻I specialize in crafting efficient and performant systems.℻My journey in tech is driven by curiosity and ecstasy for development.℻℗")
   .concat("℻℗℻commands: (click to run)")
   .replace(/℗/g, " ").replace(/℻/g, "\n")
 
